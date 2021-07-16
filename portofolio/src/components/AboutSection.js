bdcbb7a57f9c420eaebb5dd0c84f54c918a1da1c
Dino Santic <dino@everest.ba>
2021-07-16 15:03:12 +0200

    components styleing pt.1

--- portofolio/src/components/AboutSection.js
+++ portofolio/src/components/AboutSection.js
@@ -1,28 +1,61 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//styled components
+import styled from "styled-components";
+
 const AboutSection = () => {
   return (
-    <div className="description">
-      <div className="title">
-        <div className="hide">
-          <h2>We work to make</h2>
-        </div>
-        <div className="hide">
-          <h2>
-            your <span>dreams</span> come
-          </h2>
-        </div>
-        <div className="hide">
-          <h2>true.</h2>
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
         </div>
         <p>Contact us for photograpy and videography.</p>
         <button>Contact us</button>
-        <div className="image">
-          <img src={home1} alt="guy with a camera" />
-        </div>
-      </div>
-    </div>
+      </Description>
+      <Image>
+        <img src={home1} alt="guy with a camera" />
+      </Image>
+    </About>
   );
 };
 
+//Styled components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
 export default AboutSection;
