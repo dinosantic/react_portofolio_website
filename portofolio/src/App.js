bd12a9b58f7d803e58ecbec817147f586d6e8b91
Dino Santic <dino@everest.ba>
2021-07-28 14:21:51 +0200

    router

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -1,16 +1,32 @@
-import AboutUs from "./pages/AboutUs"
-import Nav from "./components/Nav"
 //global style
-import GlobalStyle from "./components/GlobalStyle"
+import GlobalStyle from "./components/GlobalStyle";
+//pages
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//components
+import Nav from "./components/Nav";
+//router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<div className='App'>
-			<GlobalStyle />
-			<Nav />
-			<AboutUs />
-		</div>
-	)
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- portofolio/src/components/Nav.js
+++ portofolio/src/components/Nav.js
@@ -1,42 +1,53 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Nav = () => {
-	return (
+  return (
     <StyledNav>
-        <h1><a id='logo' href="#">Capture</a></h1>
-        <ul>
-            <li><a href="#">1. About Us</a></li>
-            <li><a href="#">2. Our Work</a></li>
-            <li><a href="#">3. Contact Us</a></li>
-        </ul>
+      <h1>
+        <Link id="logo" to="/">
+          Capture
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">1. About Us</Link>
+        </li>
+        <li>
+          <Link to="/work">2. Our Work</Link>
+        </li>
+        <li>
+          <Link to="/contact">3. Contact Us</Link>
+        </li>
+      </ul>
     </StyledNav>
-    )
-}
+  );
+};
 const StyledNav = styled.nav`
-min-height: 10vh;
-display: flex;
-margin: auto;
-justify-content: space-between;
-align-items: center;
-padding: 1rem 5rem;
-background: #282828;
-a {
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 5rem;
+  background: #282828;
+  a {
     color: white;
     text-decoration: none;
-}
-ul {
+  }
+  ul {
     display: flex;
     list-style: none;
-}
-#logo {
+  }
+  #logo {
     font-size: 1.5rem;
-    font-family: 'Lobster', cursive;
+    font-family: "Lobster", cursive;
     font-weight: lighter;
-}
-li {
+  }
+  li {
     padding-left: 10rem;
     position: relative;
-}
-`
-export default Nav
+  }
+`;
+export default Nav;
