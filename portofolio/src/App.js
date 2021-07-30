89c1f9bab3633da8908e3da67613d937b464090c
Dino Santic <dino@everest.ba>
2021-07-30 12:40:59 +0200

    MovieDetails component

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -4,6 +4,7 @@ import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetails from "./pages/MovieDetails";
 //components
 import Nav from "./components/Nav";
 //router
@@ -18,8 +19,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetails />
         </Route>
         <Route path="/contact">
           <ContactUs />
